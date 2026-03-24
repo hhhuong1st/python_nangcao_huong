c67d3bb11c2d67656aaf6e76e120ceb28e306305
H <huynhhuyenhuong2006@gmail.com>
2026-03-24 15:56:23 +0700

    h

--- qlsv/csdl_qlsv.py
+++ qlsv/csdl_qlsv.py
@@ -1,4 +1,6 @@
 import tkinter as tk
+from tkinter import ttk, messagebox
+import sqlite3
 
 class quanlySVApp:
     def __init__(self,root):
@@ -13,11 +15,116 @@ class quanlySVApp:
         self.hien_thi_du_lieu()
 
     def khoi_tao_db(self):
-        pass
+        conn = sqlite3.connect('quanlySV.db')
+        cur = conn.cursor()
+        cur.execute(''' 
+                        CREATE TABLE IF NOT EXISTS SinhVien (
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    ten TEXT NOT NULL,
+                    tuoi INTEGER,
+                    nganh TEXT
+                    )
+                ''')
+        conn.commit()
+        conn.close()
+
+
     def tao_giao_dien(self):
-        pass
+        frame_nhap = tk.Frame(self.root,padx=20,pady=20)
+        frame_nhap.pack(side=tk.LEFT, fill=tk.Y)
+        tk.Label(frame_nhap, text="THÔNG TIN SINH VIÊN",
+                 font=("Arial", 14,"bold"), fg="blue").grid(row=0, column=0,columnspan=2, pady=10)
+        
+        tk.Label(frame_nhap, text="Họ và tên").grid(row=1, column=0, sticky=tk.W,pady=5)
+        self.txt_ten = tk.Entry(frame_nhap, width=25)
+        self.txt_ten.grid(row=1, column=1, pady=5)
+
+        tk.Label(frame_nhap, text="Tuổi: ").grid(row=2, column=0, sticky=tk.W,pady=5)
+        self.txt_tuoi = tk.Entry(frame_nhap, width=25)
+        self.txt_tuoi.grid(row=2, column=1, pady=5)
+
+        tk.Label(frame_nhap, text="Ngành Học: ").grid(row=3, column=0, sticky=tk.W, pady=5)
+        self.cb_nganh = ttk.Combobox(frame_nhap, width=22, state="readonly",
+                                     value = ("Công nghệ thông tin", "Kế toán", "Quản trị kinh doanh", "Ngôn ngữ Anh"))
+        self.cb_nganh.grid(row=3, column=1, pady=5)
+
+        # Nút
+        frame_nut = tk.Frame(frame_nhap)
+        frame_nut.grid(row=4, column=0, columnspan=2, pady=20)
+
+        tk.Button(frame_nut, text="Thêm", bg="#90ee90", width=8, command=self.them_sv).grid(row=4, column=0, sticky=tk.W, padx=5)
+        tk.Button(frame_nut, text="Sửa", bg="#f5f36f", width=8, command=self.sua_sv).grid(row=4, column=1, sticky=tk.W, padx=5)
+        tk.Button(frame_nut, text="Xoá", bg="#ec857d", width=8, command=self.xoa_sv).grid(row=4, column=2, sticky=tk.W, padx=5)
+        tk.Button(frame_nut, text="Làm mới", bg="#65d7fa", width=8, command=self.lam_moi).grid(row=4, column=3, sticky=tk.W, padx=5)
+
+        frame_bang = tk.Frame(self.root,padx=10, pady=10)
+        frame_bang.pack (side=tk.RIGHT, fill=tk.BOTH, expand=True)
+        scrollbar = tk.Scrollbar(frame_bang)
+        scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
+        self.bang_sv = ttk.Treeview(frame_bang, columns=("ID", "Ten", "Tuoi", "Nganh"),
+                                    show="headings", yscrollcommand=scrollbar.set)
+        scrollbar.config(command=self.bang_sv.yview)
+
+        self.bang_sv.column("ID", width=40, anchor=tk.CENTER)
+        self.bang_sv.column("Ten", width=150, anchor=tk.W)
+        self.bang_sv.column("Tuoi", width=50, anchor=tk.CENTER)
+        self.bang_sv.column("Nganh", width=120, anchor=tk.CENTER)
+
+        self.bang_sv.heading("ID", text="Mã SV")
+        self.bang_sv.heading("Ten", text="Họ và tên")
+        self.bang_sv.heading("Tuoi", text="Tuổi")
+        self.bang_sv.heading("Nganh", text="Ngành học")
+
+        self.bang_sv.pack(fill=tk.BOTH, expand=True)
+
+        self.bang_sv.bind("<ButtonRelease-1>", self.chon_dong_bang)
+    
+    def chon_dong_bang(self, event):
+        dong_chon = self.bang_sv.focus()
+        if not dong_chon:
+            return
+        gia_tri = self.bang_sv.item(dong_chon, 'values')
+        self.txt_ten.insert(0, gia_tri[1])
+        self.txt_tuoi.insert(0, gia_tri[2])
+        self.cb_nganh.insert(0, gia_tri[3])
+
+    def lam_moi()
+        
     def hien_thi_du_lieu(self):
+        for row in self.bang_sv.get_children():
+            self.bang_sv.delete(row)
+
+        conn = sqlite3.connect("quanlySV.db")
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM SinhVien")
+        for row in cur.fetchall():
+            self.bang_sv.insert('', tk.END, values=row)
+        conn.close()
+
+    def them_sv(self):
+        ten = self.txt_ten.get()
+        tuoi = self.txt_tuoi.get()
+        nganh = self.cb_nganh.get()
+
+        if not ten or not tuoi or not nganh:
+            messagebox.showwarning("Lỗi", "Vui lòng nhập đủ thông tin")
+            return
+        conn = sqlite3.connect('quanlySV.db')
+        cur = conn.cursor()
+        cur.execute("INSERT INTO SinhVien (ten, tuoi, nganh) VALUES (?, ?, ?)", (ten, tuoi, nganh))
+        conn.commit()
+        conn.close()
+       
+        self.hien_thi_du_lieu()
+        messagebox.showinfo("Thành công", f"Đã thêm: {ten}")
+
+    def sua_sv(self):
         pass
+
+    def xoa_sv(self):
+        pass
+
+    
 
 
 if __name__ =="__main__":
